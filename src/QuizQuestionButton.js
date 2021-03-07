fbf1da6e59b0855c7f576eec7789e73ef2a5ecb7
Vinay <vinayrpatel0000@gmail.com>
2021-03-07 20:22:14 +0530

    module 2

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class QuizQuestionButton extends Component {
+  render() {
+    return (
+      <li>
+        <button></button>
+      </li>
+    );
+  }
+}
+export default QuizQuestionButton;
